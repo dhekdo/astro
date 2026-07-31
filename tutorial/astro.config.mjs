5205d4445ae0bb328753d566a3c1b7248d322ebd
dhekdo <dhekdo@naver.com>
2026-07-31 16:39:44 +0900

    new08

--- tutorial/astro.config.mjs
+++ tutorial/astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import preact from '@astrojs/preact';
 
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dhekdo.github.io',
-  base: '/astro',
+  site: isGithubActions ? 'https://dhekdo.github.io' : 'https://dhekdo-astro.netlify.app',
+  base: isGithubActions ? '/astro' : '/',
   integrations: [preact()],
 });
